41fa7b8952751252fdf081e424da794638470f3f
Dania Irshad <daniairshad@live.com>
2021-11-22 21:50:05 -0600

    completed testing

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,11 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  expect(calculateMonthlyPayment({amount: 500, years: 3, rate: 5})).toEqual('14.99');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  expect(calculateMonthlyPayment({amount: 542.9832399, years: 3, rate: 5})).toEqual('16.27');
 });
 
 /// etc
--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       update();
     });
@@ -25,27 +25,27 @@ function setupIntialValues() {
   document.getElementById('loan-years').value = 3
   document.getElementById('loan-rate').value = 5
   update();
-}
+  }
 
-// Get the current values from the UI
-// Update the monthly payment
-function update() {
-  let values = getCurrentUIValues();
-  updateMonthly(calculateMonthlyPayment(values));
-}
+  // Get the current values from the UI
+  // Update the monthly payment
+  function update() {
+    let values = getCurrentUIValues();
+    updateMonthly(calculateMonthlyPayment(values));
+  }
 
-// Given an object of values (a value has amount, years and rate ),
-// calculate the monthly payment.  The output should be a string
-// that always has 2 decimal places.
-function calculateMonthlyPayment(values) {
-  const rate = (values.rate / 100) / 12;
-  const payments = Math.floor(values.years * 12);
-  let monthlyPayment = ((rate * values.amount) / (1 - Math.pow((1 + rate), -payments))).toFixed(2)
-  return monthlyPayment;
-}
+  // Given an object of values (a value has amount, years and rate ),
+  // calculate the monthly payment.  The output should be a string
+  // that always has 2 decimal places.
+  function calculateMonthlyPayment(values) {
+    const rate = (values.rate / 100) / 12;
+    const payments = Math.floor(values.years * 12);
+    let monthlyPayment = ((rate * values.amount) / (1 - Math.pow((1 + rate), -payments))).toFixed(2)
+    return monthlyPayment;
+  }
 
-// Given a string representing the monthly payment value,
-// update the UI to show the value.
-function updateMonthly(monthly) {
-  document.getElementById('monthly-payment').innerText = monthly;
-}
+  // Given a string representing the monthly payment value,
+  // update the UI to show the value.
+  function updateMonthly(monthly) {
+    document.getElementById('monthly-payment').innerText = monthly;
+  }
